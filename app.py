9da7c5ba6c1f6d4edd2dbb3c5e4136f65446810c
suv789 <suvajitbera789@gmail.com>
2026-08-30 11:30:00 +0530

    Update backgroud color of the image

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def add_bg_from_local(image_file):
             left: 0;
             right: 0;
             bottom: 0;
-            background: rgba(0, 0, 0, 0.7);
+            background: rgba(0, 0, 0, 0.5);
             z-index: 1;
         }}
         .main {{
